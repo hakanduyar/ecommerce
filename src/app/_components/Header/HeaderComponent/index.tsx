91604eec25b1e2479b6318725238161682b68d71
rikords <iamhakanduyar@gmail.com>
2024-03-05 20:22:21 +0300

    Update header component styles and structure

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,13 +3,15 @@ import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 
+import classes from './index.module.scss'
+
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
     <div>
-      <nav>
-        <Gutter>
+      <nav className={[classes.header]}>
+        <Gutter className={classes.wrap}>
           <Link href="/">
-            <img src="/logo-black.svg" alt="logo" />
+            <img src="/logo-black.svg" alt="logo" className={classes.logo} />
           </Link>
         </Gutter>
       </nav>
