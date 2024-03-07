06d286714442d5b5579722b8eceb5245059004fb
rikords <iamhakanduyar@gmail.com>
2024-03-07 11:20:45 +0300

    Add footer component to all pages except the ones listed in `noHeaderFooterUrls`.

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,9 +1,20 @@
+'use client'
 import React from 'react'
 
 import classes from './index.module.scss'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+import { Gutter } from '../../Gutter'
 
 const FooterComponent = () => {
-  return <div>Footer</div>
+  const pathname = usePathname()
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inclusions}></ul>
+      </Gutter>
+    </footer>
+  )
 }
 
 export default FooterComponent
